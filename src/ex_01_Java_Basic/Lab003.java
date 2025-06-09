1266236c4b2b3de9b40f5c0c77a50897433454b6
sahil <shaikhsahilss2022@gmail.com>
2025-06-09 07:40:22 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Lab003.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab003 {
+
+    public static void main(String[] args) {
+
+        String num = "1,2,3,4,5-1,2,-3,-10";
+        String a [] = num.split(",");
+
+        System.out.println("Shaikh Sahil");
+        //This is a comment, this code will not be executed
+    }
+}
