8f12ce1849357cf9b5c3d1a36dac4691968b7aae
sahil <shaikhsahilss2022@gmail.com>
2025-06-10 08:04:21 +0530

    Initial commit

--- src/ex_01_Java_Basic/Lab003.java
+++ src/ex_01_Java_Basic/Lab003.java
@@ -1,13 +1,23 @@
 package ex_01_Java_Basic;
 
+import java.util.*;
+
 public class Lab003 {
 
     public static void main(String[] args) {
 
-        String num = "1,2,3,4,5-1,2,-3,-10";
-        String a [] = num.split(",");
+        String num = "1,2,3,4,5,-1,2,-3,-10";
+        String[] a = num.split(",");
 
-        System.out.println("Shaikh Sahil");
+        List<Integer> a1 = new ArrayList<>();
+
+        for (String b : a){
+            a1.add(Integer.parseInt(b));
+        }
+
+        Collections.sort(a1);
+        System.out.println(a1);
+
         //This is a comment, this code will not be executed
     }
 }
