77218d61f163ca9e9d16224c0581dda032e1638f
sahil <shaikhsahilss2022@gmail.com>
2025-06-19 07:58:32 +0530

    19th June

---
+++ src/Coding_test/challenage_3.java
@@ -0,0 +1,17 @@
+package Coding_test;
+
+public class challenage_3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+ boolean m =  a==b;
+        boolean d =  a>b;
+        boolean e =  a!=b;
+        boolean f =  a<b;
+        boolean g =  a<=b;
+        boolean h =  a>=b;
+        boolean ch = m || e || d && f && g || h;
+        System.out.println(ch);
+
+    }
+}
