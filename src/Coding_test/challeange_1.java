9dabd3dfef04e832508de0f2255d40c3bb103911
sahil <shaikhsahilss2022@gmail.com>
2025-06-19 07:44:34 +0530

    19th June

---
+++ src/Coding_test/challeange_1.java
@@ -0,0 +1,16 @@
+package Coding_test;
+
+public class challeange_1 {
+    public static void main(String[] args) {
+        int a = 25;
+        double b = 3.14;
+        boolean c = 5<10;
+        char ch = 'A';
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println(c);
+        System.out.println(ch);
+
+    }
+}
